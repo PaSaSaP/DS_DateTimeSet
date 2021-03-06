5f14b598fcd49e68059096d9480de5173eede50a
PaSaSaP <PaSaSaP@users.noreply.github.com>
2021-03-06 10:46:57 +0100

    Add script for setting date in DS sensors.

---
+++ ustaw_date.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+BAUD=9600
+DEVICE=/dev/tty.usbserial-11240
+ESCAPE_FILE=escape.txt
+
+SCRIPT=script_file.txt
+rm -f $SCRIPT
+DATE=$(date -v+5S +"%H %M %S %w %m %d %y")
+cat <<EOF >${SCRIPT}
+sleep 5
+send G ${DATE}
+sleep 1
+print Set date to: ${DATE}
+EOF
+CAPTURE=capture.txt
+rm -f ${CAPTURE}
+sudo minicom -b ${BAUD} -D ${DEVICE} -C ${CAPTURE} -S ${SCRIPT} <${ESCAPE_FILE}
+cat ${CAPTURE}
+
